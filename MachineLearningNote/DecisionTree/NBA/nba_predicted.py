c5c523e1e490171a714c2cbf67abe7a3e1fee4f9
leafage <majialongcode@gmail.com>
2018-01-31 23:51:41 +0800

    Update

--- MachineLearningNote/DecisionTree/NBA/nba_predicted.py
+++ MachineLearningNote/DecisionTree/NBA/nba_predicted.py
@@ -10,13 +10,15 @@ import pandas as pd
 
 def load_dataset():
     """
-    加载数据及
+    加载数据集
     :return:
     """
     data_filename = 'leagues_NBA_2014_games_games.csv'
-    dataset = pd.read_csv(data_filename)
-    print(dataset.ix[:5])
+    dataset = pd.read_csv(data_filename, parse_dates=['Date'])
+    return dataset
 
 
 if __name__ == '__main__':
-    load_dataset()
+    dataset = load_dataset()
+    print(dataset['HomeWin'])
+
