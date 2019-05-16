235475c83cb8349c8a40662f309d367827cd3655
jiajia <majialongcode@gmail.com>
2019-05-16 22:28:43 +0800

    mnist scikit-learn

--- MachineLearningNote/Hands-OnMLWithScikit-LearnandTensorFlow/03_mnist.py
+++ MachineLearningNote/Hands-OnMLWithScikit-LearnandTensorFlow/03_mnist.py
@@ -15,6 +15,10 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
 
 mnist = fetch_mldata('MNIST original')
 X, y = mnist['data'], mnist['target']
@@ -59,7 +63,57 @@ y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 result = confusion_matrix(y_train_5, y_train_pred)
 
-print(result)
+# print(precision_score(y_train_5, y_train_pred))
+# print(recall_score(y_train_5, y_train_pred))
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method='decision_function')
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+y_train_pred_90 = (y_scores > 70000)
+# print(y_scores)
 
-print(precision_score(y_train_5, y_train_pred))
-print(recall_score(y_train_5, y_train_pred))
+
+# 绘画出recall和precision 的图像
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.clf()
+    plt.plot(thresholds, precisions[: -1], "b--", label='Precision')
+    plt.plot(thresholds, recalls[: -1], "g-", label='Recall')
+    plt.xlabel('Threshold')
+    plt.legend(loc='upper left')
+    plt.ylim([0, 1])
+    plt.show()
+
+
+# plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+
+# 绘画ROC
+def plot_roc_curve(fpr, tpr, label=None):
+    # plt.clf()
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc='bottom right')
+    plt.show()
+
+
+# plot_roc_curve(fpr, tpr)
+# print(roc_auc_score(y_train_5, y_scores))
+
+# 利用随机森林进行预测
+forest_clf = RandomForestClassifier(random_state=42)
+# predict_proba 返回一个数组，每行代表一个实例，每列表示一个类别， 其中（i，j） 代表 第i个实例属于j类的概率
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method='predict_proba')
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+
+plt.clf()
+plt.plot(fpr, tpr, 'b:', label='SGD')
+plot_roc_curve(fpr_forest, tpr_forest, 'Random Forest')
+
+
+
+
+
+
