22c2f5ac26ea43e06f15b778747db113fc2bdae0
jiajia <majialongcode@gmail.com>
2019-07-03 16:09:19 +0800

    cats vs dogs

---
+++ DeepLearningWithPython/05_dogs_vs_cats.py
@@ -0,0 +1,172 @@
+# @Time    : 2019/6/9 16:26
+# @Author  : Leafage
+# @File    : dogs_vs_cats.py
+# @Software: PyCharm
+# @Describe: Kaggle dogs vs cats ，《Python深度学习》中的实现方法
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+
+# 搭建网络
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# 把图像转换为像素张量，并且随机转换增强数据，获取更多的数据集
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,)
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+    r'F:\GitRepository\MachineLearningNote\DeepLearningWithPython\datasets\cats_and_dogs_small\train',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary'
+)
+validation_generator = test_datagen.flow_from_directory(
+    r'F:\GitRepository\MachineLearningNote\DeepLearningWithPython\datasets\cats_and_dogs_small\validation',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary'
+)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+model.save('cats_and_dogs_small_2.h5')
+
+print(history.history.keys())
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and Validation loss')
+plt.legend()
+
+plt.show()
+
+"""
+import os, shutil
+# 使用2000张图片作为数据集
+# The path to the directory where the original
+# dataset was uncompressed
+original_dataset_dir = r''
+
+# The directory where we will
+# store our smaller dataset
+base_dir = r'F:\GitRepository\MachineLearningNote\DeepLearningWithPython\datasets\cats_and_dogs_small'
+os.mkdir(base_dir)
+
+# Directories for our training,
+# validation and test splits
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+# Directory with our validation cat pictures
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+# Directory with our validation dog pictures
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+# Copy first 1000 cat images to train_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 cat images to validation_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 cat images to test_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy first 1000 dog images to train_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 dog images to validation_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 dog images to test_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+"""
