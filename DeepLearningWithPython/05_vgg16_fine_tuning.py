62c9be75631ee8e66a291a698a9f594d97b354d3
jiajia <majialongcode@gmail.com>
2019-07-03 11:06:42 +0800

    vgg16_fine_tuning

--- DeepLearningWithPython/05_vgg16_fine_tuning.py
+++ DeepLearningWithPython/05_vgg16_fine_tuning.py
@@ -27,3 +27,90 @@ for layer in conv_base.layers:
         layer.trainable = True
     else:
         layer.trainable = False
+
+# 准备数据 ， 使用数据增强
+base_dir = r'F:\GitRepository\MachineLearningNote\DeepLearningWithPython\datasets\cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,  # 数值缩放在0-1之间
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1./255)  # 验证数据不能增强
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),  # 将所有图片调整为150*150
+    batch_size=20,  # 把数据分为若干批次，每批20个
+    class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+# 构建模型
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,  # 调整训练批次
+    validation_data=validation_generator,
+    validation_steps=50)
+
+# 绘制结果
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+
+# 平滑曲线用的
+def smooth_curve(points, factor=0.8):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1-factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+
+plt.plot(epochs, smooth_curve(acc), 'bo', label='Smoothed training acc')
+plt.plot(epochs, smooth_curve(val_acc), 'b', label='Smoothed validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, smooth_curve(loss), 'bo', label='Smoothed training loss')
+plt.plot(epochs, smooth_curve(val_loss), 'b', label='Smoothed validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+plt.savefig(r'F:\GitRepository\MachineLearningNote\DeepLearningWithPython\results\05_vgg16_fine_tuning.png', format='png')
