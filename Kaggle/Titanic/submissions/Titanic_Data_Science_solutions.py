ca753e9321d67c9f67f41c76d62c2eba66fb8b2c
jiajia <majialongcode@gmail.com>
2019-08-15 11:05:49 +0800

    My first Kaggle submission。

--- Kaggle/Titanic/submissions/Titanic_Data_Science_solutions.py
+++ Kaggle/Titanic/submissions/Titanic_Data_Science_solutions.py
@@ -147,7 +147,7 @@ for dataset in combine:
 rs = train_df[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived',
                                                                                                      ascending=False)
 
-# 统计是个独自一人
+# 统计是独自一人的特征
 for dataset in combine:
     dataset['IsAlone'] = 0
     dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
@@ -180,16 +180,112 @@ test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
 
 # 划分为4个等级
 train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
-rs = train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True)
+rs = train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand',
+                                                                                                 ascending=True)
 
 # 转化为ordinal
 for dataset in combine:
-    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
     dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
-    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
-    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
     dataset['Fare'] = dataset['Fare'].astype(int)
 
 train_df = train_df.drop(['FareBand'], axis=1)
 combine = [train_df, test_df]
 
+# 至此，所有数据已全部转化为数值型
+# print(train_df.head())
+
+# 使用模型进行训练预测
+X_train = train_df.drop('Survived', axis=1)
+Y_train = train_df['Survived']
+X_test = test_df.drop('PassengerId', axis=1).copy()
+
+# 使用Logistic Regression
+logreg = LogisticRegression()
+logreg.fit(X_train, Y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+# print(acc_log)  # 81.26
+
+# 计算feature之间的相关性
+coeff_df = pd.DataFrame(train_df.columns.delete(0))  # 删除Survived这一列
+coeff_df.columns = ['Feature']
+coeff_df["Correlation"] = pd.Series(logreg.coef_[0])
+rs = coeff_df.sort_values(by='Correlation', ascending=False)  # 按照相关性从上往下排序
+
+# acc每次计算均有波动
+# 使用SVM
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+# print(acc_svc)  # 83.5
+
+# 使用KNN， n=3
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+# print(acc_knn)  # 84.06
+
+# 使用高斯朴素贝叶斯
+gaussian = GaussianNB()
+gaussian.fit(X_train, Y_train)
+Y_pred = gaussian.predict(X_test)
+acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+# print(acc_gaussian)  # 76.88
+
+# 使用感知机
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+# print(acc_perceptron)  # 78.79
+
+# 使用Linear SVC
+linear_svc = LinearSVC()
+linear_svc.fit(X_train, Y_train)
+Y_pred = linear_svc.predict(X_test)
+acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
+# print(acc_linear_svc)  # 79.46
+
+# 使用SGD
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+# print(acc_sgd)  # 76.66
+
+# 使用决策树
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+# print(acc_decision_tree)  # 86.64
+
+# 使用随机森林
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+# print(acc_random_forest)  # 86.64
+
+# Model evaluation
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+              'Random Forest', 'Naive Bayes', 'Perceptron',
+              'Stochastic Gradient Decent', 'Linear SVC',
+              'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log,
+              acc_random_forest, acc_gaussian, acc_perceptron,
+              acc_sgd, acc_linear_svc, acc_decision_tree]})
+rs = models.sort_values(by='Score', ascending=False)
+
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred  # random forest 的预测结果
+    })
+
+submission.to_csv('../datasets/my_submission.csv', index=False)
